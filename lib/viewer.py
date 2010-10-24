ad3fd87f4a122b5dd3e624081cedf810700274e8
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-10-24 21:13:42 +0300

    Add QmlApplicationViewer into own module

---
+++ lib/viewer.py
@@ -0,0 +1,33 @@
+# QML App viewer class
+# Based on Qt Creator -created QmlApplicationViewer C++ class
+# Licensed under the LGPL
+class QmlApplicationViewer(QtDeclarative.QDeclarativeView):
+    Orientation = Enum('LockPortrait', 'LockLandscape', 'Auto')
+
+    def __init__(self, parent=None):
+        QtDeclarative.QDeclarativeView.__init__(self)
+        QtCore.QObject.connect(self.engine(), QtCore.SIGNAL('quit()'), self, QtCore.SLOT('close()'))
+        self.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
+        self.mainQmlFile = ""
+
+    def setMainQmlFile(self, filename):
+        self.mainQmlFile = QmlApplicationViewer._adjustPath(filename)
+        self.setSource(QtCore.QUrl.fromLocalFile(self.mainQmlFile))
+
+    def addImportPath(self,  path):
+        pass
+
+    def setOrientation(self, orientation):
+        pass
+
+    @classmethod
+    def _adjustPath(cls, path):
+        pathInShareDir = os.path.join(
+            QtCore.QCoreApplication.applicationDirPath(),
+            "/../share/",
+            QtCore.QFileInfo(QtCore.QCoreApplication.applicationFilePath()).fileName(),
+            path)
+        if QtCore.QFileInfo(pathInShareDir).exists():
+            return pathInShareDir
+        return path
+
