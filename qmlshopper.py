4ce1320a2204034e23c328e4c1b4e21d05506562
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-10-24 21:13:59 +0300

    Move QmlApplicationViewer into its own module

--- qmlshopper.py
+++ qmlshopper.py
@@ -1,37 +1,8 @@
 #!/usr/bin/env python
 import sys, os
 from PySide import QtGui, QtCore, QtDeclarative
-from enum import Enum
-
-class QmlApplicationViewer(QtDeclarative.QDeclarativeView):
-    Orientation = Enum('LockPortrait', 'LockLandscape', 'Auto')
-
-    def __init__(self, parent=None):
-        QtDeclarative.QDeclarativeView.__init__(self)
-        QtCore.QObject.connect(self.engine(), QtCore.SIGNAL('quit()'), self, QtCore.SLOT('close()'))
-        self.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
-        self.mainQmlFile = ""
-
-    def setMainQmlFile(self, filename):
-        self.mainQmlFile = QmlApplicationViewer._adjustPath(filename)
-        self.setSource(QtCore.QUrl.fromLocalFile(self.mainQmlFile))
-
-    def addImportPath(self,  path):
-        pass
-
-    def setOrientation(self, orientation):
-        pass
-
-    @classmethod
-    def _adjustPath(cls, path):
-        pathInShareDir = os.path.join(
-            QtCore.QCoreApplication.applicationDirPath(),
-            "/../share/",
-            QtCore.QFileInfo(QtCore.QCoreApplication.applicationFilePath()).fileName(),
-            path)
-        if QtCore.QFileInfo(pathInShareDir).exists():
-            return pathInShareDir
-        return path
+from lib.enum import Enum
+from lib.viewer import QmlApplicationViewer
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
