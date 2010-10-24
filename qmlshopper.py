c9c12713bc32366ff99ee3181a41a043c060b29f
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-10-24 21:57:59 +0300

    Implement simple model class

--- qmlshopper.py
+++ qmlshopper.py
@@ -3,11 +3,29 @@ import sys, os
 from PySide import QtGui, QtCore, QtDeclarative
 from lib.viewer import QmlApplicationViewer
 
+class WantedListModel(QtCore.QAbstractListModel):
+    def __init__(self, parent=None):
+        QtCore.QAbstractListModel.__init__(self, parent)
+        self._things = []
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        if not parent.isValid():
+            return len(self._things)
+    
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            return QtCore.QVariant(self._things[index.row])
+        elif role == QtCore.Qt.BackgroundRole:
+            return QVariant()
+    
+    @QtCore.Slot
+    def addThing(self, thing):
+        self.beginInsertRows(QtCore.QModelIndex(), len(self._things), len(self._things))
+        self._things.append(thing)
+        self.endInsertRows()
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    #view = QtDeclarative.QDeclarativeView()
-    #view.setSource(QtCore.QUrl('qml/qmlshopper/main.qml'))
-    #view.setResizeMode(QtDeclarative.QDeclarativeView.SizeRootObjectToView)
     view = QmlApplicationViewer()
     view.setMainQmlFile('qml/qmlshopper/main.qml')
     
