5da484e54853f8b3656e45cd1efce9a23a74e685
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-10-24 16:02:53 +0300

    An unit test for the wanted model

---
+++ unittest/wantedlistmodel/tst_wantedlistmodeltest.cpp
@@ -0,0 +1,45 @@
+#include <QtCore/QString>
+#include <QtTest/QtTest>
+#include <QtCore/QCoreApplication>
+
+class WantedListModelTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    WantedListModelTest();
+
+private Q_SLOTS:
+    void initTestCase();
+    void cleanupTestCase();
+    void rowCount();
+    void rowCount_data();
+};
+
+WantedListModelTest::WantedListModelTest()
+{
+}
+
+void WantedListModelTest::initTestCase()
+{
+}
+
+void WantedListModelTest::cleanupTestCase()
+{
+}
+
+void WantedListModelTest::rowCount()
+{
+    QFETCH(QString, data);
+    QVERIFY2(true, "Failure");
+}
+
+void WantedListModelTest::rowCount_data()
+{
+    QTest::addColumn<QString>("data");
+    QTest::newRow("0") << QString();
+}
+
+QTEST_MAIN(WantedListModelTest);
+
+#include "tst_wantedlistmodeltest.moc"
---
+++ unittest/wantedlistmodel/wantedlistmodel.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-10-20T22:30:17
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+QT       -= gui
+
+TARGET = tst_wantedlistmodeltest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_wantedlistmodeltest.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
+
+INCLUDEPATH += -I../../lib
