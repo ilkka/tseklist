fa7d4f9921afec8c2853bc0890356694d8123305
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-10-27 18:24:57 +0300

    Use a list of WantedListItems instead of model class

--- qmlshopper.py
+++ qmlshopper.py
@@ -2,22 +2,25 @@
 import sys, os, logging
 from PySide import QtGui, QtCore, QtDeclarative
 from lib.viewer import QmlApplicationViewer
-from lib.model import WantedListModel
+from lib.model import WantedListItem
 
 logging.basicConfig(level=logging.DEBUG)
+
+def addThing(thing):
+    logging.debug("Would add thing \"%s\"" % thing)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     view = QmlApplicationViewer()
 
-    model = WantedListModel()
-    view.rootContext().setContextProperty("wantedlist", model)
+    items = [WantedListItem('This'), WantedListItem('That'), WantedListITem('Whatnot')]
+    view.rootContext().setContextProperty("wantedlist", items)
 
     view.setMainQmlFile('qml/qmlshopper/main.qml')
 
     root = view.rootObject()
     QtCore.QObject.connect(root, QtCore.SIGNAL('addThing(QString)'),
-                           model.addThing)
+                           addThing)
 
     view.show()
     sys.exit(app.exec_())
