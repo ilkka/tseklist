4dd179a94a9887ed952cbd18c0039c5a9d93fe30
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-02 20:09:38 +0200

    Set a QObjectList as the model

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <QtGui/QApplication>
 #include "qmlapplicationviewer.h"
+#include <QObjectList>
+#include "lib/wantedlistitem.h"
+#include <QDeclarativeContext>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QObjectList* o = new QObjectList();
+    o->append(new WantedListItem());
+    o->append(new WantedListItem());
+
     QmlApplicationViewer viewer;
+
+    viewer.rootContext()->setContextProperty("wantedlist", o);
+
     viewer.setOrientation(QmlApplicationViewer::Auto);
     viewer.setMainQmlFile(QLatin1String("qml/qmlshopper/main.qml"));
     viewer.show();
