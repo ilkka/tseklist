69e78fd9854f99790b8a701bed0c63b8fd35c26c
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-02 19:59:54 +0200

    Create tested object on heap

--- unittest/wantedlistitem/tst_wantedlistitemtest.cpp
+++ unittest/wantedlistitem/tst_wantedlistitemtest.cpp
@@ -19,20 +19,20 @@ WantedListItemTest::WantedListItemTest()
 
 void WantedListItemTest::testSetTitle()
 {
-    WantedListItem i = WantedListItem();
-    i.set_title("moro");
-    QCOMPARE("moro", i.get_title());
-    i.set_title("tere");
-    QCOMPARE("tere", i.get_title());
+    WantedListItem* i = new WantedListItem(this);
+    i->set_title("moro");
+    QCOMPARE("moro", i->get_title());
+    i->set_title("tere");
+    QCOMPARE("tere", i->get_title());
 }
 
 void WantedListItemTest::testSetStatus()
 {
-    WantedListItem i = WantedListItem();
-    i.set_status(0);
-    QCOMPARE(0, i.get_status());
-    i.set_status(1);
-    QCOMPARE(1, i.get_status());
+    WantedListItem* i = new WantedListItem(this);
+    i->set_status(0);
+    QCOMPARE(0, i->get_status());
+    i->set_status(1);
+    QCOMPARE(1, i->get_status());
 }
 
 DECLARE_TEST(WantedListItemTest)
