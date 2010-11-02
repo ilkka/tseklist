33abfda8891880008927b47c8a21534c0ecf1a1f
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-02 19:33:12 +0200

    Write some tests for item

--- unittest/wantedlistitem/tst_wantedlistitemtest.cpp
+++ unittest/wantedlistitem/tst_wantedlistitemtest.cpp
@@ -1,5 +1,6 @@
 #include <QtTest>
 #include "common/AutoTest.h"
+#include "lib/wantedlistitem.h"
 
 class WantedListItemTest : public QObject
 {
@@ -18,12 +19,20 @@ WantedListItemTest::WantedListItemTest()
 
 void WantedListItemTest::testSetTitle()
 {
-
+    i = WantedListItem();
+    i.set_title("moro");
+    QCOMPARE("moro", i.get_title());
+    i.set_title("tere");
+    QCOMPARE("tere", i.get_title());
 }
 
 void WantedListItemTest::testSetStatus()
 {
-
+    i = WantedListItem();
+    i.set_status(0);
+    QCOMPARE(0, i.get_status());
+    i.set_status(1);
+    QCOMPARE(1, i.get_status());
 }
 
 DECLARE_TEST(WantedListItemTest)
